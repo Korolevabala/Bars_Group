22776d132f90d71990dae899d672400c93d56ca0
Korolevbala <pamitdura228@gmail.com>
2021-03-10 20:48:17 +0300

    сделал вашу бибу

---
+++ Ex3/Class_Car_Bars_group/Class_Car_Bars_group/Class1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Class_Car_Bars_group
+{
+    public class Car
+    {
+        //атрибуты
+        private Motor motor = null;
+        private Body body = null;
+        private Oil oil = null; 
+        private Fabric fabric = null;
+        void InitializationCar()
+        {
+            this.motor = new Motor();
+            this.body = new Body();
+            this.oil = new Oil();
+            this.fabric = new Fabric();
+        }
+        //.ctor
+        public Car(string name)
+        {
+            InitializationCar();
+            this.fabric.Name = name;
+        }
+        //method
+        public void Show()
+        {
+            this.body.Show();
+            this.fabric.Show();
+            this.motor.Show();
+            this.oil.Show();
+        }
+
+        public string Body
+        {
+            set
+            {
+                this.body.Content = value;
+            }
+        }
+
+        public string Motor
+        {
+            set
+            {
+                this.motor.Content = value;
+            }
+        }
+
+        public string Oil
+        {
+            set
+            {
+                this.oil.Content = value;
+            }
+        }
+    }
+    class Oil
+    {
+        private string content;
+
+        public string Content
+        {
+            private get
+            {
+                if (content != null)
+                    return content;
+                else
+                {
+                    return "sosi";
+                }
+            }
+            set
+            {
+                content = value;
+            }
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Content);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+    }
+    class Body
+    {
+        private string content;
+
+        public string Content
+        {
+            private get
+            {
+                if (content != null)
+                    return content;
+                else
+                {
+                    return "sosi";
+                }
+            }
+            set
+            {
+                content = value;
+            }
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Content);
+            Console.ForegroundColor = ConsoleColor.Red;
+        } 
+    }
+    class Motor
+    {
+        private string content;
+
+        public string Content
+        {
+            private get
+            {
+                if (content != null)
+                    return content;
+                else
+                {
+                    return "sosi";
+                }
+            }
+            set
+            {
+                content = value;
+            }
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(Content);
+            Console.ForegroundColor = ConsoleColor.White;
+        } 
+    }
+    class Fabric
+    {
+        private string name;
+
+        public string Name
+        {
+            private get
+            {
+                if (name != null)
+                    return name;
+                else
+                {
+                    return "sosi";
+                }
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Name);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        } 
+    }
+}
---
+++ Ex3/exechnik/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Class_Car_Bars_group;
+
+namespace exechnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("BMW");
+            car.Body = "3";
+            car.Motor = "120 л.с.";
+            car.Oil = "95";
+            car.Show();
+            Console.ReadKey();
+        }
+    }
+}
