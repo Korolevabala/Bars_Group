b281321442ef9e4436f9280b789646fe1ba8f993
Korolevbala <pamitdura228@gmail.com>
2021-03-10 22:17:12 +0300

    пример неработающего кода на f#

---
+++ quick sort/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let square x = x * x
+let getSquares items =
+   items |> List.map square
+   
+let lst =[1;2;3;4;5]
+printf "квадрат числа %A равен %A" lst (getSquares lst)
+//let rec quicksort =
+   // function
+   // | [] -> []
+    //| x::xs ->
+       // let lesser = List.filter((>) x) xs
+       // let greater = List.filter((<=) x) xs
+       // (quicksort lesser) @ [x] @ (quicksort greater)
